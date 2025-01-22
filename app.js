d38e713dce66ed3cbadab15450c3e3f4e6940441
caileighsmith <62355730+caileighsmith@users.noreply.github.com>
2025-01-22 14:56:10 +0000

    Implemented first AI lesson for 5 languages, introducing first 10  words.

--- app.js
+++ app.js
@@ -3,15 +3,33 @@ const express = require('express');
 const cors = require('cors');
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
+require('dotenv').config();
+
+
 //Server definitions:
 const app = express();
 const port = process.env.PORT || 3000;
 
 //AI definitions:
-const geminiKey = process.env.GEMINI_KEY;
+const geminiKey = process.env.GEMINI_KEY
 const genAI = new GoogleGenerativeAI(geminiKey)
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" })
 const supportedLanguages = ['hebrew', 'german', 'russian', 'spanish', 'french']
+
+const startClass = async(language)=>{
+    try {
+        const prompt = `Hi, your job is to be a language tutor. This is the first ever lesson that our student will take, assume they know nothing. The language chosen is: ${language}. Introduce the first 10 words for the user and return it in a list, with english meanings, and nothing else. Your response should JUST be the list. Provide transliterations too, so we know what the words sound like`
+        const result = await model.generateContent(prompt)
+        const response = result.response.text()
+        return response
+    } catch (error){
+        console.error(`Error generating content: ${error}`)
+    }
+}
+
+const createFlashCard = async (wordsArray)=>{
+    //In this function, we take in an array of words, and allow the user to create flashcards for each word in the array
+}
 
 
 app.use(cors());
@@ -20,7 +38,7 @@ app.get('', (req, res)=>{
     res.send('hi')
 })
 
-app.get('/language/:languageName', async(req, res)=>{
+app.get('/learn/:languageName', async(req, res)=>{
 
     const language = req.params.languageName.toLowerCase()
 
@@ -28,8 +46,11 @@ app.get('/language/:languageName', async(req, res)=>{
         return res.status(400).send({error: `Language '${language}' not supported.`})
     }
 
+    const response = await startClass(language)
+    console.log(response)
     res.send({
-        message: language
+        message: `${language} is supported!`,
+        firstClass: response
     })
 
 })
