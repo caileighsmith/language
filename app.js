455767ab7700c20a5fbca6a627f6c9f4bd93e8d3
caileighsmith <62355730+caileighsmith@users.noreply.github.com>
2025-01-24 11:54:13 +0000

    ++cors and language learning functions

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 require('dotenv').config();
 
-
 //Server definitions:
 const app = express();
 const port = process.env.PORT || 3000;
+
+//Enabling cross origin resource sharing (cors):
+app.use(cors())
 
 //AI definitions:
 const geminiKey = process.env.GEMINI_KEY
@@ -27,15 +29,34 @@ const startClass = async(language)=>{
     }
 }
 
+const generateAiTheme = async(language, languageTheme, learningLevel) => {
+
+    try{
+        const prompt = `Based on the theme '${languageTheme}', your job is to be a language tutor. This is the first ever lesson that our student will take, assume they know nothing. The language chosen is: ${language}. Introduce the first 10 words for the user and return it in a list, with english meanings, and nothing else. Your response should JUST be the list. Provide transliterations too, so we know what the words sound like.`;
+        const result = await model.generateContent(prompt);
+        const response = result.response.text()
+        return response;
+    } catch (error){
+        console.error(`Error generating content: ${error}`)
+    }
+
+}
+
 const createFlashCard = async (wordsArray)=>{
     //In this function, we take in an array of words, and allow the user to create flashcards for each word in the array
 }
 
-
-app.use(cors());
-
-app.get('', (req, res)=>{
-    res.send('hi')
+app.get('/', async (req, res)=>{
+    
+    //Testing the generateAiTheme function:
+    const data = await generateAiTheme("russian", "sports")
+    if (!data){
+        return res.status(400).send({error: `No data provided.`})
+    }
+    
+    res.send({
+        response: data
+    })
 })
 
 app.get('/learn/:languageName', async(req, res)=>{
@@ -54,7 +75,6 @@ app.get('/learn/:languageName', async(req, res)=>{
     })
 
 })
-
 
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}`)
